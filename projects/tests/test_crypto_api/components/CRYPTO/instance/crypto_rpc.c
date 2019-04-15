58a7203bca7c78cd477d9f1191d44764d4864786
Carmelo Pintaudi <c.pintaudi@gmail.com>
2019-04-15 13:33:38 +0200

    [DEV] SEOS-43: renaming accorging to the schema <ModuleName_functionExported>

--- projects/tests/test_crypto_api/components/CRYPTO/instance/crypto_rpc.c
+++ projects/tests/test_crypto_api/components/CRYPTO/instance/crypto_rpc.c
@@ -1,13 +1,13 @@
-#include "seos/crypto_rpc.h"
+#include "seos/SeosCryptoRpc.h"
 #include "LibDebug/Debug.h"
 
 #include <camkes.h>
 
 seos_err_t
-crypto_rpc_get_instance(crypto_rpc_handle_t* instance)
+crypto_rpc_get_instance(SeosCryptoRpc_Handle* instance)
 {
-    static crypto_rpc_ctx_t the_one;
-    seos_err_t retval = crypto_rpc_init(&the_one, server_dataport);
+    static SeosCryptoRpc the_one;
+    seos_err_t retval = SeosCryptoRpc_ctor(&the_one, server_dataport);
     *instance = &the_one;
 
     Debug_LOG_DEBUG("%s: created rpc object %p", __func__, *instance);
--- projects/tests/test_crypto_api/components/MAIN/src/main.c
+++ projects/tests/test_crypto_api/components/MAIN/src/main.c
@@ -3,20 +3,20 @@
 #include <stdio.h>
 #include <camkes.h>
 
-#include "seos/crypto_client.h"
+#include "seos/SeosCryptoClient.h"
 
 int run()
 {
-    crypto_client_t client;
-    crypto_rpc_handle_t rpc_handle = NULL;
+    SeosCryptoClient client;
+    SeosCryptoRpc_Handle rpc_handle = NULL;
 
     void const* data;
 
     crypto_rpc_get_instance(&rpc_handle);
     Debug_LOG_DEBUG("%s: got rpc object %p from server", __func__, rpc_handle);
 
-    crypto_client_init(&client, rpc_handle, crypto_client_dataport);
-    crypto_client_get_random_data(&client, 0, &data, 0);
+    SeosCryptoClient_ctor(&client, rpc_handle, crypto_client_dataport);
+    SeosCryptoClient_getRandomData(&client, 0, &data, 0);
 
     return 0;
 }
