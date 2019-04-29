298615d763336619fecea7159d092f9ca6c615fc
Carmelo Pintaudi <c.pintaudi@gmail.com>
2019-04-29 15:31:20 +0200

    [DEV] SEOS-175: prepare main.c with comments that explain the concept of a basic test of ProxyNVM

--- projects/tests/test_proxy_nvm/src/main.c
+++ projects/tests/test_proxy_nvm/src/main.c
@@ -2,6 +2,15 @@
 
 int run()
 {
-    printf("hello world!\n");
+// This is how the basic test will look like
+//    ProxyNVM nvm;
+//
+//    bool ok = ProxyNVM_ctor(&nvm, ...);
+//    // write something, and print it out
+//    NVM_write(ProxyNVM_TO_NVM(&nvm), ...)
+//    Debug_LOG_INFO(...)
+//    // read it back and print out what was read
+//    NVM_read(ProxyNVM_TO_NVM(&nvm), ...)
+//    Debug_LOG_INFO(...)
     return 0;
 }
