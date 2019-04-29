1fcdd134bbcf72dd6957903e9ff22c1e1747d9f7
Carmelo Pintaudi <c.pintaudi@gmail.com>
2019-04-29 15:28:57 +0200

    [DEV] SEOS-175: add ChanMux and Uart components and bind all togheter

---
+++ projects/tests/test_proxy_nvm/components/ChanMux/src/instance/ChanMux_Instance.c
@@ -0,0 +1,159 @@
+/*
+ *  WAN/LAN/NetworkStack Channel MUX
+ *
+ *  Copyright (C) 2019, Hensoldt Cyber GmbH
+ */
+
+#include "ChanMux/ChanMux.h"
+#include "ChanMux_config.h"
+#include "seos/seos_err.h"
+#include "assert.h"
+#include <camkes.h>
+
+static uint8_t mainFifoBuf[PAGE_SIZE];
+
+static const ChanMuxConfig_t cfgChanMux = {
+    .numChannels = CHANMUX_NUM_CHANNELS,
+    .outputDataport = {
+        .io  = (void**) &outputDataPort,
+        .len = PAGE_SIZE
+    },
+    .channelsFifos = {
+        {   // Channel 0
+            .buffer = mainFifoBuf,
+            .len = sizeof(mainFifoBuf)
+        }
+    }
+};
+
+const ChannelDataport_t dataports[] = {
+    {
+        .io  = (void**) &mainDataPort,
+        .len = PAGE_SIZE
+    }
+};
+
+//------------------------------------------------------------------------------
+const ChanMuxConfig_t*
+ChanMux_config_getConfig(void)
+{
+    return &cfgChanMux;
+}
+
+//------------------------------------------------------------------------------
+void
+ChanMux_dataAvailable_emit(unsigned int chanNum)
+{
+    Debug_LOG_TRACE("%s: chan %u",
+                    __func__, chanNum);
+    switch (chanNum)
+    {
+        case CHANNEL_MAIN_DATA:
+            dataAvailableMain_emit();
+        break;
+
+        default:
+            Debug_LOG_ERROR("%s(): invalid channel %u", __func__, chanNum);
+            break;
+    }
+}
+
+//------------------------------------------------------------------------------
+static ChanMux*
+ChanMux_getInstance(void)
+{
+    // singleton
+    static ChanMux  theOne;
+    static ChanMux* self = NULL;
+    static Channel_t channels[CHANMUX_NUM_CHANNELS];
+
+    if ((NULL == self) && ChanMux_ctor(&theOne,
+                                       channels,
+                                       ChanMux_config_getConfig(),
+                                       ChanMux_dataAvailable_emit,
+                                       Output_write))
+    {
+        self = &theOne;
+    }
+
+    return self;
+}
+
+void
+ChanMuxOut_takeByte(char byte)
+{
+    ChanMux_takeByte(ChanMux_getInstance(), byte);
+}
+
+//==============================================================================
+// CAmkES Interface
+//==============================================================================
+
+//------------------------------------------------------------------------------
+seos_err_t
+ChanMuxIn_write(
+    unsigned int  chanNum,
+    size_t        len,
+    size_t*       lenWritten)
+{
+    Debug_LOG_TRACE("%s(): channel %u, len %u", __func__, chanNum, len);
+
+    // set defaults to error
+    *lenWritten = 0;
+
+    const ChannelDataport_t* dp = NULL;
+    switch (chanNum)
+    {
+        //---------------------------------
+        case CHANNEL_MAIN_DATA:
+            dp = &dataports[chanNum];
+            break;
+        //---------------------------------
+        default:
+            Debug_LOG_ERROR("%s(): invalid channel %u", __func__, chanNum);
+            return SEOS_ERROR_ACCESS_DENIED;
+    }
+
+    Debug_ASSERT( NULL != dp );
+    seos_err_t ret = ChanMux_write(ChanMux_getInstance(), chanNum, dp, &len);
+    *lenWritten = len;
+
+    Debug_LOG_TRACE("%s(): channel %u, lenWritten %u", __func__, chanNum, len);
+
+    return ret;
+}
+
+
+//------------------------------------------------------------------------------
+seos_err_t
+ChanMuxIn_read(
+    unsigned int  chanNum,
+    size_t        len,
+    size_t*       lenRead)
+{
+    Debug_LOG_TRACE("%s(): channel %u, len %u", __func__, chanNum, len);
+
+    // set defaults to error
+    *lenRead = 0;
+
+    const ChannelDataport_t* dp = NULL;
+    switch (chanNum)
+    {
+        //---------------------------------
+        case CHANNEL_MAIN_DATA:
+            dp = &dataports[chanNum];
+            break;
+        //---------------------------------
+        default:
+            Debug_LOG_ERROR("%s(): invalid channel %u", __func__, chanNum);
+            return SEOS_ERROR_ACCESS_DENIED;
+    }
+
+    Debug_ASSERT( NULL != dp );
+    seos_err_t ret = ChanMux_read(ChanMux_getInstance(), chanNum, dp, &len);
+    *lenRead = len;
+
+    Debug_LOG_TRACE("%s(): channel %u, lenRead %u", __func__, chanNum, len);
+
+    return ret;
+}
---
+++ projects/tests/test_proxy_nvm/components/ChanMux/src/instance/ChanMux_config.h
@@ -0,0 +1,20 @@
+/*
+ *  WAN/LAN Channel MUX
+ *
+ *  Copyright (C) 2019, Hensoldt Cyber GmbH
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum {
+    CHANNEL_MAIN_DATA,      // 0
+    CHANMUX_NUM_CHANNELS    // 1
+};
+
+#ifdef __cplusplus
+}
+#endif
