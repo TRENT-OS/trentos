054d2640d06c653698c86a0f38e05346ec516421
Leonard <Leonhard.Blazevic@hensoldt-cyber.com>
2019-05-02 13:29:46 +0200

    [DEV]SEOS-175:ProxyNVM implementation foundation, ChanMux changes
    Added a temporary fix to channel initialization so that we are able to use just 1 channel (not necessarily the first one) and skip the rest of them, ground work for the ProxyNVM test application in main.

--- projects/tests/test_proxy_nvm/components/ChanMux/src/instance/ChanMux_config.h
+++ projects/tests/test_proxy_nvm/components/ChanMux/src/instance/ChanMux_config.h
@@ -11,8 +11,15 @@ extern "C" {
 #endif
 
 enum {
-    CHANNEL_MAIN_DATA,      // 0
-    CHANMUX_NUM_CHANNELS    // 1
+    CHANNEL_LAN_DATA,       // 0
+    CHANNEL_WAN_DATA,       // 1
+    CHANNEL_LAN_CTRL,       // 2
+    CHANNEL_WAN_CTRL,       // 3
+    CHANNEL_NW_STACK_CTRL,  // 4
+    CHANNEL_NW_STACK_DATA,  // 5
+    CHANNEL_MAIN_DATA,      // 6
+
+    CHANMUX_NUM_CHANNELS    // 7
 };
 
 #ifdef __cplusplus
--- projects/tests/test_proxy_nvm/src/main.c
+++ projects/tests/test_proxy_nvm/src/main.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
+#include "ProxyNVM.h"
+#include "ChanMux/ChanMuxClient.h"
+#include "camkes.h"
+
+ProxyNVM testProxyNVM;
+ChanMuxClient testChanMuxClient;
+
+const char buf[4] = {0x0a, 0x0b, 0x0c, 0x0d};
+char input_buf[4] = {0};
 
 int run()
 {
-// This is how the basic test will look like
-//    ProxyNVM nvm;
-//
-//    bool ok = ProxyNVM_ctor(&nvm, ...);
-//    // write something, and print it out
-//    NVM_write(ProxyNVM_TO_NVM(&nvm), ...)
-//    Debug_LOG_INFO(...)
-//    // read it back and print out what was read
-//    NVM_read(ProxyNVM_TO_NVM(&nvm), ...)
-//    Debug_LOG_INFO(...)
+    printf("Starting main...\n");
+
+    bool success = ChanMuxClient_ctor(&testChanMuxClient, 6, (void*)chanMuxDataPort);
+
+    if(!success){
+        printf("Failed to construct testChanMuxClient!\n");
+        return 0;
+    }
+
+    success = ProxyNVM_ctor(&testProxyNVM, &testChanMuxClient);
+
+    if(!success){
+        printf("Failed to construct testProxyNVM!\n");
+        return 0;
+    }
+
+    size_t written_bytes = ProxyNVM_write(ProxyNVM_TO_NVM(&testProxyNVM), (size_t)(0x12345678), buf, sizeof(buf));
+    size_t read_bytes = ProxyNVM_read(ProxyNVM_TO_NVM(&testProxyNVM), (size_t)(0x12345678), input_buf, sizeof(input_buf));
+
+    printf("Written bytes: %d\nRead bytes: %d\n", written_bytes, read_bytes);
+    printf("Read memory:");
+
+    for(int i = 0; i < read_bytes; i++){
+        printf("\n  %d", input_buf[i]);
+    }
+
     return 0;
 }
