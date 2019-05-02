ee0ece7c731ae30567f6ad95083b7005b84460d4
Leonard <Leonhard.Blazevic@hensoldt-cyber.com>
2019-05-02 16:38:35 +0200

    [DEV]SEOS-175:Simple write test for the ProxyNVM.

--- projects/tests/test_proxy_nvm/src/main.c
+++ projects/tests/test_proxy_nvm/src/main.c
@@ -6,12 +6,15 @@
 ProxyNVM testProxyNVM;
 ChanMuxClient testChanMuxClient;
 
-const char buf[4] = {0x0a, 0x0b, 0x0c, 0x0d};
-char input_buf[4] = {0};
+char buf[1074*2] = {0};
 
 int run()
 {
     printf("Starting main...\n");
+
+    for(int i = 0; i < sizeof(buf); i++){
+        buf[i] = i%127;
+    }
 
     bool success = ChanMuxClient_ctor(&testChanMuxClient, 6, (void*)chanMuxDataPort);
 
@@ -27,15 +30,10 @@ int run()
         return 0;
     }
 
-    size_t written_bytes = ProxyNVM_write(ProxyNVM_TO_NVM(&testProxyNVM), (size_t)(0x12345678), buf, sizeof(buf));
-    size_t read_bytes = ProxyNVM_read(ProxyNVM_TO_NVM(&testProxyNVM), (size_t)(0x12345678), input_buf, sizeof(input_buf));
+    size_t written_bytes = ProxyNVM_write(ProxyNVM_TO_NVM(&testProxyNVM), (size_t)(0x12345678), (const char*)buf, sizeof(buf));
+    //size_t read_bytes = ProxyNVM_read(ProxyNVM_TO_NVM(&testProxyNVM), (size_t)(0x12345678), input_buf, sizeof(input_buf));
 
-    printf("Written bytes: %d\nRead bytes: %d\n", written_bytes, read_bytes);
-    printf("Read memory:");
-
-    for(int i = 0; i < read_bytes; i++){
-        printf("\n  %d", input_buf[i]);
-    }
+    printf("Written bytes: %d\n", written_bytes);
 
     return 0;
 }
