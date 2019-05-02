e977d988eccbe882abe705d91b11e823860f92fb
Carmelo Pintaudi <c.pintaudi@gmail.com>
2019-05-02 11:48:43 +0200

    [DEV] SEOS-43: test_crypto_api, print more output

--- projects/tests/test_crypto_api/components/MAIN/src/main.c
+++ projects/tests/test_crypto_api/components/MAIN/src/main.c
@@ -16,7 +16,17 @@ int run()
     Debug_LOG_DEBUG("%s: got rpc object %p from server", __func__, rpcHandle);
 
     SeosCryptoClient_ctor(&client, rpcHandle, cryptoClientDataport);
-    SeosCryptoClient_getRandomData(&client, 0, &data, 0);
+
+    for (int i = 0; i < 10; i++)
+    {
+        SeosCryptoClient_getRandomData(&client, 0, &data, 16);
+        Debug_PRINTF("Printing random bytes...");
+        for (unsigned j = 0; j < 16; j++)
+        {
+            Debug_PRINTF(" 0x%02x", ((char*) data)[j]);
+        }
+        Debug_PRINTF("\n");
+    }
 
     return 0;
 }
