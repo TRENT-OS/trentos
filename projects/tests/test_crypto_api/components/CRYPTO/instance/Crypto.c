0af7c6d2df6bfb047073d290c30744a979ed3261
Carmelo Pintaudi <c.pintaudi@gmail.com>
2019-05-02 11:46:49 +0200

    [DEV] SEOS-43: build SeosCrypto object and pass it to the SeosCryptoRpc_ctor

--- projects/tests/test_crypto_api/components/CRYPTO/instance/Crypto.c
+++ projects/tests/test_crypto_api/components/CRYPTO/instance/Crypto.c
@@ -5,12 +5,21 @@
 seos_err_t
 Crypto_getRpcHandle(SeosCryptoRpc_Handle* instance)
 {
+    static SeosCrypto    cryptoCore;
     static SeosCryptoRpc the_one;
-    seos_err_t retval = SeosCryptoRpc_ctor(&the_one, serverDataport);
-    *instance = &the_one;
 
-    Debug_LOG_DEBUG("%s: created rpc object %p", __func__, *instance);
+    seos_err_t retval = SeosCrypto_init(&cryptoCore,
+                                        malloc, free, NULL, NULL);
+    if (SEOS_SUCCESS == retval)
+    {
+        retval = SeosCryptoRpc_ctor(&the_one, &cryptoCore, serverDataport);
+        *instance = &the_one;
 
+        if (SEOS_SUCCESS == retval)
+        {
+            Debug_LOG_TRACE("%s: created rpc object %p", __func__, *instance);
+        }
+    }
     return retval;
 }
 
