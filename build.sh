b5c99127a1699da8aff3c94ca8c91eafd7a2a1b9
Carmelo Pintaudi <c.pintaudi@gmail.com>
2019-05-20 14:27:15 +0200

    [BUILD] Add 'clean' target

--- build.sh
+++ build.sh
@@ -109,12 +109,14 @@ function run_build_mode()
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 if [[ "${1:-}" == "all" ]]; then
-
     shift
     run_build_mode zynq7000 Debug HELLO_WORLD $@
     run_build_mode zynq7000 Debug TEST_SYSLOG $@
     run_build_mode zynq7000 Debug TEST_CRYPTO_API $@
     run_build_mode zynq7000 Debug TEST_PROXY_NVM $@
+elif [[ "${1:-}" == "clean" ]]; then
+    shift
+    /bin/rm -rf build-*
 else
     run_build_mode zynq7000 Debug $@
 fi
