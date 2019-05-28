3cc00ad6209d79d0a9ee2fcd9fc974477d264fec
Leonard <Leonhard.Blazevic@hensoldt-cyber.com>
2019-05-28 14:02:05 +0200

    [DEV] SEOS-207: Adapted the main to follow to the changes in the seos_libs (CryptoBlockDevice, SeosSpiffs)

--- projects/tests/test_spiffs_integration/components/MAIN/src/main.c
+++ projects/tests/test_spiffs_integration/components/MAIN/src/main.c
@@ -1,55 +1,32 @@
 #include <stdio.h>
 #include "ProxyNVM.h"
 #include "CryptoBlockDevice.h"
-#include "spiffs.h"
+#include "SeosSpiffs.h"
 #include "ChanMux/ChanMuxClient.h"
 #include "camkes.h"
 
 #define MEM_SIZE            1024*128
-#define LOG_PAGE_SIZE       256
-
-char test_in_buf[4096] = {0};
 
 ProxyNVM testProxyNVM;
 ChanMuxClient testChanMuxClient;
 CryptoBlockDevice testCryptoBlockDevice;
-static spiffs fs;
+SeosSpiffs fs;
 
-static uint8_t spiffs_work_buf[LOG_PAGE_SIZE*2];
-static uint8_t spiffs_fds[32*4];
-static uint8_t spiffs_cache_buf[(LOG_PAGE_SIZE+32)*4];
+static bool InitProxyNVM(){
+    bool success = ChanMuxClient_ctor(&testChanMuxClient, 6, (void*)chanMuxDataPort);
 
-static int32_t spiffsRead(uint32_t addr, uint32_t size, uint8_t *dst) {
-    size_t readBytes = CryptoBlockDevice_read(&testCryptoBlockDevice, addr, (char*)dst, size);
-
-    if(readBytes != size){
-      Debug_LOG_ERROR("%s: Tried to read %d bytes, but read %d, from adress %d!", __func__, size, readBytes, addr);
-      return SPIFFS_ERR_INTERNAL;
+    if(!success){
+        Debug_LOG_ERROR("%s: Failed to construct testChanMuxClient!", __func__);
+        return false;
     }
 
-    return SPIFFS_OK;
-}
+    success = ProxyNVM_ctor(&testProxyNVM, &testChanMuxClient);
 
-static int32_t spiffsWrite(uint32_t addr, uint32_t size, uint8_t *src) {
-    size_t writtenBytes = CryptoBlockDevice_write(&testCryptoBlockDevice, addr, (char*)src, size);
-
-    if(writtenBytes != size){
-      Debug_LOG_ERROR("%s: Tried to write %d bytes, but written %d, to adress %d!", __func__, size, writtenBytes, addr);
-      return SPIFFS_ERR_INTERNAL;
+    if(!success){
+        Debug_LOG_ERROR("%s: Failed to construct testProxyNVM!", __func__);
+        return false;
     }
-
-    return SPIFFS_OK;
-}
-
-static int32_t spiffsErase(uint32_t addr, uint32_t size) {
-    size_t erasedBytes = CryptoBlockDevice_erase(&testCryptoBlockDevice, addr, size);
-
-    if(erasedBytes != size){
-      Debug_LOG_ERROR("%s: Tried to erase %d bytes, but erased %d, from adress %d!", __func__, size, erasedBytes, addr);
-      return SPIFFS_ERR_INTERNAL;
-    }
-
-    return SPIFFS_OK;
+    return true;
 }
 
 static void printSpiffsContent(){
@@ -60,7 +37,7 @@ static void printSpiffsContent(){
 
   printf("\nSpiffs content:\n");
 
-  SPIFFS_opendir(&fs, "/", &d);
+  SPIFFS_opendir(&fs.spif_fs, "/", &d);
 
   while ((pe = SPIFFS_readdir(&d, pe))) {
     printf("%s [%04x] size:%i\n", pe->name, pe->obj_id, pe->size);
@@ -69,54 +46,10 @@ static void printSpiffsContent(){
   SPIFFS_closedir(&d);
 
   printf("\nChecking the file system...\n");
-  if(SPIFFS_info(&fs, &total, &used) < 0){
-    printf("%s: SPIFFS_info error %i", __func__, SPIFFS_errno(&fs));
+  if(SPIFFS_info(&fs.spif_fs, &total, &used) < 0){
+    Debug_LOG_ERROR("%s: SPIFFS_info error %i", __func__, SPIFFS_errno(&fs.spif_fs));
   }
   printf("\nSPIFFS total available bytes: %d\nSPIFFS used bytes: %d\n", total, used);
-}
-
-static int spiffsMount(){
-    spiffs_config cfg;
-    cfg.phys_size = 128*1024;           // use 128k of spi flash
-    cfg.phys_addr = 0;                  // start spiffs at start of spi flash
-    cfg.phys_erase_block = 8*4096;      // according to datasheet
-    cfg.log_block_size = 8*4096;        // let us not complicate things
-    cfg.log_page_size = LOG_PAGE_SIZE;  // as we said
-        
-    cfg.hal_read_f = spiffsRead;
-    cfg.hal_write_f = spiffsWrite;
-    cfg.hal_erase_f = spiffsErase;
-        
-    int res = SPIFFS_mount(&fs, &cfg, spiffs_work_buf, spiffs_fds, sizeof(spiffs_fds), spiffs_cache_buf, sizeof(spiffs_cache_buf), 0);
-
-    if(res != 0){
-        Debug_LOG_INFO("%s: First try unsuccessful! Unmounting...", __func__);
-        SPIFFS_unmount(&fs);
-        Debug_LOG_INFO("%s: Formatting...", __func__);
-        SPIFFS_format(&fs);
-        Debug_LOG_INFO("%s: Mounting...", __func__);
-        res = SPIFFS_mount(&fs, &cfg, spiffs_work_buf, spiffs_fds, sizeof(spiffs_fds), spiffs_cache_buf, sizeof(spiffs_cache_buf), 0);
-    }
-
-    return res;
-}
-
-static bool InitProxyNVM(){
-    printf("\n\n");
-    bool success = ChanMuxClient_ctor(&testChanMuxClient, 6, (void*)chanMuxDataPort);
-
-    if(!success){
-        printf("Failed to construct testChanMuxClient!\n");
-        return false;
-    }
-
-    success = ProxyNVM_ctor(&testProxyNVM, &testChanMuxClient);
-
-    if(!success){
-        printf("Failed to construct testProxyNVM!\n");
-        return false;
-    }
-    return true;
 }
 
 static void runSpiffsTest(){
@@ -133,37 +66,35 @@ static void runSpiffsTest(){
 
   printSpiffsContent();
 
-
   printf("\nCreating a file with first %d numbers...\n", 10);
-  fd = SPIFFS_open(&fs, "numbers", SPIFFS_CREAT | SPIFFS_TRUNC | SPIFFS_RDWR, 0);
+  fd = SPIFFS_open(&fs.spif_fs, "numbers", SPIFFS_CREAT | SPIFFS_TRUNC | SPIFFS_RDWR, 0);
   if (fd < 0) {
-    printf("%s: SPIFFS_open error %i", __func__, SPIFFS_errno(&fs));
+    Debug_LOG_ERROR("%s: SPIFFS_open error %i", __func__, SPIFFS_errno(&fs.spif_fs));
   }
-  if (SPIFFS_write(&fs, fd, outNumbers, sizeof(outNumbers)) < 0) {
-    printf("%s: SPIFFS_write error %i", __func__, SPIFFS_errno(&fs));
+  if (SPIFFS_write(&fs.spif_fs, fd, outNumbers, sizeof(outNumbers)) < 0) {
+    Debug_LOG_ERROR("%s: SPIFFS_write error %i", __func__, SPIFFS_errno(&fs.spif_fs));
   }
-  if (SPIFFS_close(&fs, fd) < 0) {
-    printf("%s: SPIFFS_close error %i", __func__, SPIFFS_errno(&fs));
+  if (SPIFFS_close(&fs.spif_fs, fd) < 0) {
+    Debug_LOG_ERROR("%s: SPIFFS_close error %i", __func__, SPIFFS_errno(&fs.spif_fs));
 
   }
   printSpiffsContent();
 
 
   printf("\nReading the file with first %d numbers...\n", 10);
-  fd = SPIFFS_open(&fs, "numbers", SPIFFS_RDWR, 0);
+  fd = SPIFFS_open(&fs.spif_fs, "numbers", SPIFFS_RDWR, 0);
   if (fd < 0) {
-    printf("%s: SPIFFS_open error %i", __func__, SPIFFS_errno(&fs));
+    Debug_LOG_ERROR("%s: SPIFFS_open error %i", __func__, SPIFFS_errno(&fs.spif_fs));
   }
-  if (SPIFFS_read(&fs, fd, inMessage, 21) < 0) {
-    printf("%s: SPIFFS_read error %i", __func__, SPIFFS_errno(&fs));
+  if (SPIFFS_read(&fs.spif_fs, fd, inMessage, 21) < 0) {
+    Debug_LOG_ERROR("%s: SPIFFS_read error %i", __func__, SPIFFS_errno(&fs.spif_fs));
   }
-  if (SPIFFS_close(&fs, fd) < 0) {
-    printf("%s: SPIFFS_close error %i", __func__, SPIFFS_errno(&fs));
+  if (SPIFFS_close(&fs.spif_fs, fd) < 0) {
+    Debug_LOG_ERROR("%s: SPIFFS_close error %i", __func__, SPIFFS_errno(&fs.spif_fs));
   }
   printf("\nRead numbers:\n%s\n", inMessage);
 
   printf("\n--------------------DONE-----------------------");
-  printf("\n\n");
 }
 
 int run(){
@@ -172,13 +103,18 @@ int run(){
     return 0;
   }
 
-  if(!CryptoBlockDevice_ctor(&testCryptoBlockDevice, ProxyNVM_TO_NVM(&testProxyNVM), ProxyNVM_write, ProxyNVM_read, ProxyNVM_erase)){
+  if(!CryptoBlockDevice_ctor(&testCryptoBlockDevice, ProxyNVM_TO_NVM(&testProxyNVM))){
     Debug_LOG_ERROR("%s: Failed to initialize CryptoBlockDevice!", __func__);
     return 0;
   }
 
-  if(spiffsMount() != 0){
+  if(!SeosSpiffs_ctor(&fs, CryptoBlockDevice_TO_NVM(&testCryptoBlockDevice), MEM_SIZE, 0)){
     Debug_LOG_ERROR("%s: Failed to initialize spiffs!", __func__);
+    return 0;
+  }
+
+  if(!SeosSpiffs_mount(&fs)){
+    Debug_LOG_ERROR("%s: Failed to mount spiffs!", __func__);
     return 0;
   }
 
