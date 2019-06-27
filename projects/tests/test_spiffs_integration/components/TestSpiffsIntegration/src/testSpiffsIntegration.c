392e370ee03ee48fde272cff721525dc72439814
Leonard <Leonhard.Blazevic@hensoldt-cyber.com>
2019-06-27 17:45:38 +0200

    [DEV] SEOS-207: Added more tests to fully test the FileStream functionality

--- projects/tests/test_spiffs_integration/components/TestSpiffsIntegration/src/testSpiffsIntegration.c
+++ projects/tests/test_spiffs_integration/components/TestSpiffsIntegration/src/testSpiffsIntegration.c
@@ -73,35 +73,96 @@ int run(){
   }
 
   char filePath[3] = {'f', '0', '\0'};
-  char writeBuf[4] = {'c', '=', '0', '\0'};
-  char readBuf[4] = {0};
+  char writeBuf[8] = {'c', '=', '0', ',', 'b', '=', '0', '\0'};
+  char readBuf[8] = {0};
+  char getBuf[4] = {0};
 
   FileStream* streams[NUM_OF_TEST_STREAMS];
 
   Debug_LOG_DEBUG("\n--------------------------------------------------------------------------------------\n\n\n");
 
   // Performing the following test for NUM_OF_TEST_STREAMS filestreams:
-  //    1) create a filestream with a path: "fX" (X is the number of the file)
-  //    2) write dummy data to it ("c=X")
-  //    3) position the ponter to the begining of the file
-  //    4) read the written data
+  //    1)  create a filestream with a path: "fX" (X is the number of the file)
+  //    2)  write dummy data to it ("c=X")
+  //    3)  position the ponter to the begining of the file
+  //    4)  check the available space on the file
+  //    5)  reopen the file as read-only
+  //    6)  try to write to read-only file and verify this will result in an error
+  //    7)  position the ponter to the begining of the file
+  //    8)  read the written data
+  //    9)  position the ponter to the begining of the file
+  //    10) read just the first part of the file (up to the first delimiter)
+  //    11) check that there are no errors on the file
   for(int i = 0; i < NUM_OF_TEST_STREAMS; i++){
     writeBuf[2] = '0' + i + 1;
+    writeBuf[6] = '0' + i + 1;
     filePath[1] = '0' + i + 1;
 
     streams[i] = FileStreamFactory_create(streamFactory, filePath, FileStream_OpenMode_W);
 
-    if(Stream_write(FileStream_TO_STREAM(streams[i]), writeBuf, strlen(writeBuf)) < 0){
-      Debug_LOG_ERROR("%s: FileStream_write failed!", __func__);
-    }
-    if(FileStream_seek(streams[i], 0, FileStream_SeekMode_Begin) < 0){
-      Debug_LOG_ERROR("%s: FileStream_write failed!", __func__);
-    }
-    if(Stream_read(FileStream_TO_STREAM(streams[i]), readBuf, strlen(writeBuf)) < 0){
-      Debug_LOG_ERROR("%s: FileStream_write failed!", __func__);
+    if(Stream_write(FileStream_TO_STREAM(streams[i]), writeBuf, strlen(writeBuf)) <= 0)
+    {
+      Debug_LOG_ERROR("%s: Stream_write failed!", __func__);
     }
 
-    Debug_LOG_DEBUG("\nRead from file %d: %s", i + 1, readBuf);
+    if(FileStream_seek(streams[i], 0, FileStream_SeekMode_Begin) <= 0)
+    {
+      Debug_LOG_ERROR("%s: FileStream_seek failed!", __func__);
+    }
+
+    size_t ret = Stream_available(FileStream_TO_STREAM(streams[i]));
+    if(ret == sizeof(writeBuf) - 1)
+    {
+      Debug_LOG_DEBUG("\n\nAvailable space in file %d is equal to file size\n", i);
+    }
+    else
+    {
+      Debug_LOG_ERROR("%s: File %d, expected available space = %d, but available space is = %d", __func__, i, sizeof(writeBuf) - 1, ret);
+    }
+
+    streams[i] = SpiffsFileStream_reOpen(streams[i], FileStream_OpenMode_r);
+
+    if(Stream_write(FileStream_TO_STREAM(streams[i]), writeBuf, strlen(writeBuf)) <= 0)
+    {
+      Debug_LOG_DEBUG("\n\nFile %d, unsuccesful write to read-only file!\n", i);
+    }
+    else
+    {
+      Debug_LOG_ERROR("\n\nFile %d, write to read-only file succeded!\n", i);
+    }
+
+    if(FileStream_seek(streams[i], 0, FileStream_SeekMode_Begin) <= 0)
+    {
+      Debug_LOG_ERROR("%s: FileStream_seek failed!", __func__);
+    }
+
+    if(Stream_read(FileStream_TO_STREAM(streams[i]), readBuf, strlen(writeBuf)) <= 0)
+    {
+      Debug_LOG_ERROR("%s: Stream_read failed!", __func__);
+    }
+
+    if(FileStream_seek(streams[i], 0, FileStream_SeekMode_Begin) <= 0)
+    {
+      Debug_LOG_ERROR("%s: FileStream_seek failed!", __func__);
+    }
+
+    if(Stream_get(FileStream_TO_STREAM(streams[i]), getBuf, sizeof(getBuf), ",", 0) < 0)
+    {
+      Debug_LOG_ERROR("%s: Stream_get failed!", __func__);
+    }
+
+    seos_err_t err = FileStream_error(streams[i]);
+    if(err == SEOS_SUCCESS)
+    {
+      Debug_LOG_DEBUG("\n\nFile %d has no errors!\n", i);
+    }
+    else
+    {
+      Debug_LOG_DEBUG("\n\nError %d on file %d!\n", err, i);
+    }
+
+    Debug_LOG_DEBUG("\n\nRead from file %d: %s\n", i + 1, readBuf);
+    Debug_LOG_DEBUG("\n\nGet from file %d: %s\n", i + 1, getBuf);
     Debug_LOG_DEBUG("\n\n\n--------------------------------------------------------------------------------------\n\n\n");
   }
 
