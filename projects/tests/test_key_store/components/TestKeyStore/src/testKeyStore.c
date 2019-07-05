69cc461b72bc66ba6d0b80711452d3b8ddba60de
Leonard <Leonhard.Blazevic@hensoldt-cyber.com>
2019-07-05 17:22:37 +0200

    [DEV] SEOS-207: Adjusted the spiffsIntegrationTest to the changes in seos_libs, created a v1 test for keyStore.

--- projects/tests/test_key_store/components/TestKeyStore/src/testKeyStore.c
+++ projects/tests/test_key_store/components/TestKeyStore/src/testKeyStore.c
@@ -13,10 +13,10 @@
 #include "camkes.h"
 
 #define NVM_PARTITION_SIZE    (1024*128)
-#define NUM_OF_TEST_STREAMS   3
 #define CHANMUX_NVM_CHANNEL   6
-#define MASTER_KEY            "f131830db44c54742fc3f3265f0f1a0c"
-#define KEY_LENGTH_IN_BYTES   32
+#define MASTER_KEY_BYTES      "f131830db44c54742fc3f3265f0f1a0cf131830db44c54742fc3f3265f0f1a0c"
+#define MASTER_KEY_NAME       "MasterKey"
+#define MASTER_KEY_SIZE       64
 
 ProxyNVM testProxyNVM;
 ChanMuxClient testChanMuxClient;
@@ -98,16 +98,18 @@ int run()
         return 0;
     }
 
-    SeosCryptoKey key;
+    SeosCryptoKey writeKey;
     SeosCryptoKey readKey;
-    char keyBytes[33] = {0};
+    char keyBytes[MASTER_KEY_SIZE + 1] = {0};
+    size_t keysize = 0;
 
-    seos_err_t err = SeosCryptoKey_ctor(&key,
+    // create the key that is to be imported to the keyStore
+    seos_err_t err = SeosCryptoKey_ctor(&writeKey,
                              NULL,
                              SeosCryptoCipher_Algorithm_AES_CBC_ENC,
                              1 << SeosCryptoKey_Flags_IS_ALGO_CIPHER,
-                             MASTER_KEY,
-                             KEY_LENGTH_IN_BYTES * 8);
+                             MASTER_KEY_BYTES,
+                             MASTER_KEY_SIZE * 8);
 
     if (err != SEOS_SUCCESS)
     {
@@ -116,23 +118,82 @@ int run()
         return 0;
     }
 
-    err = SeosKeyStore_importKey(&keyStore, "MasterKey", &key);
+    // import the created key
+    err = SeosKeyStore_importKey(&keyStore, MASTER_KEY_NAME, &writeKey);
     if (err != SEOS_SUCCESS)
     {
         Debug_LOG_ERROR("%s: SeosKeyStore_importKey failed with error code %d!",
                         __func__, err);
         return 0;
     }
-
-    err = SeosKeyStore_getKey(&keyStore, "MasterKey", &readKey, keyBytes);
+    else
+    {
+        Debug_LOG_DEBUG("\n\nThe key is succesfully imported!\n");
+    }
+    
+    // get the size of the imported key (to be used before the getKey to allocate memory
+    // if the user does not know the size of the key)
+    err = SeosKeyStore_getKeySizeBytes(&keyStore, MASTER_KEY_NAME, &keysize);
     if (err != SEOS_SUCCESS)
     {
-        Debug_LOG_ERROR("%s: SeosKeyStore_importKey failed with error code %d!",
+        Debug_LOG_ERROR("%s: SeosKeyStore_getKeySizeBytes failed with error code %d!",
+                        __func__, err);
+        return 0;
+    }
+    else
+    {
+        // check if the size returnd by getSize is correct
+        if(keysize != MASTER_KEY_SIZE)
+        {
+            Debug_LOG_ERROR("%s: Expected keySize is %d, but the read keySize is %d! Exiting test...",
+                            __func__, MASTER_KEY_SIZE, keysize);
+            return 0;
+        }
+        Debug_LOG_DEBUG("\n\nThe keySize is succesfully read!\n");
+    }
+    
+    // read the imported key
+    err = SeosKeyStore_getKey(&keyStore, MASTER_KEY_NAME, &readKey, keyBytes);
+    if (err != SEOS_SUCCESS)
+    {
+        Debug_LOG_ERROR("%s: SeosKeyStore_getKey failed with error code %d!",
+                        __func__, err);
+        return 0;
+    }
+    else
+    {
+        // check if the key data is correct
+        if(strcmp(MASTER_KEY_BYTES, readKey.bytes) != 0 || readKey.lenBits != MASTER_KEY_SIZE*8)
+        {
+            Debug_LOG_ERROR("%s: Read key data is not correct!\n    Key bytes => %s\n    Key lenBits => %d\nExiting test...",
+                            __func__, readKey.bytes, readKey.lenBits);
+            return 0;
+        }
+        Debug_LOG_DEBUG("\n\nThe key data is succesfully read!\n");
+    }
+
+    // delete the key
+    err = SeosKeyStore_deleteKey(&keyStore, MASTER_KEY_NAME);
+    if (err != SEOS_SUCCESS)
+    {
+        Debug_LOG_ERROR("%s: SeosKeyStore_deleteKey failed with error code %d!",
                         __func__, err);
         return 0;
     }
 
-    printf("\nReadkey data\n    bytes => %s\n    lenBits => %d\n", readKey.bytes, readKey.lenBits);
+    // check if the key is actaully deleted by verifying that the getKey results in an error
+    err = SeosKeyStore_getKey(&keyStore, MASTER_KEY_NAME, &readKey, keyBytes);
+    if (err != SEOS_ERROR_NOT_FOUND)
+    {
+        Debug_LOG_ERROR("%s: Expected to receive a SEOS_ERROR_NOT_FOUND after reading the deleted key, but received an err code: %d! Exiting test...",
+                        __func__, err);
+        return 0;
+    }
+    else
+    {
+        Debug_LOG_DEBUG("\n\nThe key is succesfully deleted!\n");
+    }
+    
 
     return 0;
 }
