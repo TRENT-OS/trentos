dd9118a2ebd37ad1268a09c4b205b408cbb20ad5
Matthias Buhl <matthias.buhl@hensoldt-cyber.com>
2019-07-08 14:25:15 +0200

    [REF]SEOS-240: Took out duplicate code of the two testers and created a common file with the respective functions

--- projects/tests/test_proxy_nvm/src/ProxyNVM_Tester2.c
+++ projects/tests/test_proxy_nvm/src/ProxyNVM_Tester2.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include "ProxyNVM.h"
-#include "ChanMux/ChanMuxClient.h"
+#include "LibDebug/Debug.h"
 #include "camkes.h"
+#include "ProxyNVM_common.h"
 
-#define MEM_SIZE                        (1024*128)
+
 #define CHANMUX_NVM_CHANNEL             7
+#define MEM_SIZE                        (1024*128)
 
 #define TEST_SMALL_SECTION_LEN          (MEM_SIZE / PAGE_SIZE) //arbitrary small chunk of data
 #define TEST_WHOLE_MEM_LEN              MEM_SIZE
@@ -16,107 +17,47 @@
 #define TEST_SIZE_OUT_OF_BOUNDS_ADDR    (MEM_SIZE / 2)
 #define TEST_ADDR_OUT_OF_BOUNDS_ADDR    (MEM_SIZE * 2)
 
-ProxyNVM testProxyNVM;
-ChanMuxClient testChanMuxClient;
 
-unsigned char out_buf[MEM_SIZE] = {0};
-unsigned char in_buf[MEM_SIZE] = {0};
-
-
-void RunTest(size_t address, size_t length, const char* testName)
-{
-    printf("\n\n");
-    for (u_int i = 0; i < length; i++)
-    {
-        out_buf[i] = (char)i;
-    }
-
-    size_t ret_value = ProxyNVM_write(ProxyNVM_TO_NVM(&testProxyNVM),
-                                      address, (const char*)out_buf, length);
-
-    if (ret_value == length)
-    {
-        Debug_LOG_INFO("\nChannel %d: %s: Write succeded!", CHANMUX_NVM_CHANNEL,
-                       testName);
-    }
-    else
-    {
-        Debug_LOG_ERROR("\nChannel %d: %s: Write failed!\nTried to write %d bytes but written only %d bytes.",
-                        CHANMUX_NVM_CHANNEL, testName, length, ret_value);
-        return;
-    }
-
-    ret_value = ProxyNVM_read(ProxyNVM_TO_NVM(&testProxyNVM), address,
-                              (char*)in_buf, length);
-    if (ret_value == length)
-    {
-        Debug_LOG_INFO("\nChannel %d: %s: Read succeded!", CHANMUX_NVM_CHANNEL,
-                       testName);
-    }
-    else
-    {
-        Debug_LOG_ERROR("\nChannel %d: %s: Read failed!\nTried to read %d bytes but read only %d bytes.",
-                        CHANMUX_NVM_CHANNEL, testName, length, ret_value);
-        return;
-    }
-
-    for (int i = 0; i < length; i++)
-    {
-        if (out_buf[i] != in_buf[i])
-        {
-            Debug_LOG_ERROR("\nChannel %d: %s: Read values corrupted!\nOn position %d written %02x, but read %02x",
-                            CHANMUX_NVM_CHANNEL, testName, i, out_buf[i], in_buf[i]);
-            return;
-        }
-    }
-    Debug_LOG_INFO("\nChannel %d: %s: Read values match the write values!",
-                   CHANMUX_NVM_CHANNEL, testName);
-}
-
-int InitProxyNVM(size_t chan)
-{
-    printf("\n\n");
-    bool isSuccess = ChanMuxClient_ctor(&testChanMuxClient, chan, chanMuxDataPort);
-
-    if (!isSuccess)
-    {
-        Debug_LOG_ERROR("Failed to construct testChanMuxClient!\n");
-        return -1;
-    }
-
-    isSuccess = ProxyNVM_ctor(&testProxyNVM, &testChanMuxClient,
-                              (char*)chanMuxDataPort, PAGE_SIZE);
-
-    if (!isSuccess)
-    {
-        Debug_LOG_ERROR("Failed to construct testProxyNVM!\n");
-        return -1;
-    }
-
-    return 0;
-}
 
 int run()
 {
-    printf("\n\n");
-    Debug_LOG_INFO("Initializing ProxyNVM with channel:%d", CHANMUX_NVM_CHANNEL);
-
-    int ret_value = InitProxyNVM(CHANMUX_NVM_CHANNEL);
+    int ret_value = ProxyNVMTest_init(CHANMUX_NVM_CHANNEL);
 
     if (ret_value < 0)
     {
         Debug_LOG_ERROR("%s(): channel %u: Error initializing ProxyNVM! Errno:%d",
                         __func__, CHANMUX_NVM_CHANNEL, ret_value);
-        return 0;
+        return -1;
     }
 
-    RunTest(TEST_SMALL_SECTION_ADDR, TEST_SMALL_SECTION_LEN, "TEST SMALL SECTION");
-    RunTest(TEST_WHOLE_MEM_ADDR, TEST_WHOLE_MEM_LEN, "TEST WHOLE MEMORY");
-    RunTest(TEST_SIZE_OUT_OF_BOUNDS_ADDR, TEST_SIZE_OUT_OF_BOUNDS_LEN,
-            "TEST SIZE OUT OF BOUNDS");
-    RunTest(TEST_ADDR_OUT_OF_BOUNDS_ADDR, TEST_ADDR_OUT_OF_BOUNDS_LEN,
-            "TEST ADDRESS OUT OF BOUNDS");
+    bool isSuccess;
 
+    isSuccess = ProxyNVMTest_run(TEST_SMALL_SECTION_ADDR, TEST_SMALL_SECTION_LEN, "TEST SMALL SECTION");
+    if (!isSuccess)
+    {
+        Debug_LOG_ERROR("Failed TEST SMALL SECTION!\n");
+    }
+
+    isSuccess = ProxyNVMTest_run(TEST_WHOLE_MEM_ADDR, TEST_WHOLE_MEM_LEN, "TEST WHOLE MEMORY");
+    if (!isSuccess)
+    {
+        Debug_LOG_ERROR("Failed TEST WHOLE MEMORY!\n");
+    }
+
+    isSuccess = ProxyNVMTest_run(TEST_SIZE_OUT_OF_BOUNDS_ADDR, TEST_SIZE_OUT_OF_BOUNDS_LEN,
+            "TEST SIZE OUT OF BOUNDS");
+    if (!isSuccess)
+    {
+        Debug_LOG_ERROR("Failed TEST SIZE OUT OF BOUNDS!\n");
+    }
+
+    isSuccess = ProxyNVMTest_run(TEST_ADDR_OUT_OF_BOUNDS_ADDR, TEST_ADDR_OUT_OF_BOUNDS_LEN,
+            "TEST ADDRESS OUT OF BOUNDS");
+    if (!isSuccess)
+    {
+        Debug_LOG_ERROR("Failed TEST ADDRESS OUT OF BOUNDS!\n");
+    }
+ 
     return 0;
 
 }
