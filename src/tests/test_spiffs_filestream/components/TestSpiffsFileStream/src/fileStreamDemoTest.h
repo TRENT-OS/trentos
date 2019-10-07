a7ca47ba65d85c60e65baa2860f2fa0eb2b011d5
Leonard <Leonhard.Blazevic@hensoldt-cyber.com>
2019-10-07 14:00:45 +0200

    Astyle.

--- src/tests/test_spiffs_filestream/components/TestSpiffsFileStream/src/fileStreamDemoTest.h
+++ src/tests/test_spiffs_filestream/components/TestSpiffsFileStream/src/fileStreamDemoTest.h
@@ -12,10 +12,10 @@
 /**
  * @weakgroup Test_Spiffs_FileStream_DemoTests
  * @{
- * 
+ *
  * @brief A demo that tests a typical use case of the spiffs filestream
  *
- * @test \b Test_Spiffs_FileStream_test_case_04     \n  1)  Create a filestream with 
+ * @test \b Test_Spiffs_FileStream_test_case_04     \n  1)  Create a filestream with
  *                                                  \n  2)  Write dummy string to it
  *                                                  \n  3)  Position the pointer to the beginning of the file
  *                                                  \n  4)  Check that the available space on the file and that it is
@@ -26,10 +26,10 @@
  *                                                  \n  7)  Verify that the read string matches the written one
  *                                                  \n  8)  Position the pointer to the beginning of the file
  *                                                  \n  9)  Read just the first part of the file (up to the first delimiter)
- *                                                  \n  10) Verify that the read string matches the first part of 
+ *                                                  \n  10) Verify that the read string matches the first part of
  *                                                          the original one
  *                                                  \n  11) Delete the opened file
- * 
+ *
  * @}
  */
 seos_err_t spiffsFileStreamDemoTest_run(FileStreamFactory* fsFactory);
--- src/tests/test_spiffs_filestream/components/TestSpiffsFileStream/src/fileStreamUnitTests.h
+++ src/tests/test_spiffs_filestream/components/TestSpiffsFileStream/src/fileStreamUnitTests.h
@@ -12,7 +12,7 @@
 /**
  * @weakgroup Test_Spiffs_FileStream_UnitTests
  * @{
- * 
+ *
  * @brief Unit tests for the create filestream function
  *
  * @test \b Test_Spiffs_FileStream_test_case_01     \n <b> 1) Positive cases: </b>
@@ -21,14 +21,14 @@
  *                                                  \n          Create a filestream with a too long name
  *                                                  \n          Create a filestream with an empty string for a name
  *                                                  \n          Create a filestream with NULL passed for a name
- *                                                  \n          Try to create a filestream with mode = r, when it does not previously exist 
+ *                                                  \n          Try to create a filestream with mode = r, when it does not previously exist
  * @}
  */
 seos_err_t test_createFileStream(FileStreamFactory* fsFactory);
 /**
  * @weakgroup Test_Spiffs_FileStream_UnitTests
  * @{
- * 
+ *
  * @brief Unit tests for the write filestream function
  *
  * @test \b Test_Spiffs_FileStream_test_case_02     \n <b> 1) Positive cases: </b>
@@ -36,23 +36,23 @@ seos_err_t test_createFileStream(FileStreamFactory* fsFactory);
  *                                                  \n <b> 2) Negative cases (check for proper error codes): </b>
  *                                                  \n          Try to pass NULL as a write buffer
  *                                                  \n          Try to pass 0 as the write length
- *                                                  \n          Try to pass NULL as a write buffer and 0 as the write length 
+ *                                                  \n          Try to pass NULL as a write buffer and 0 as the write length
  * @}
  */
 seos_err_t test_writeFileStream(FileStreamFactory* fsFactory);
 /**
  * @weakgroup Test_Spiffs_FileStream_UnitTests
  * @{
- * 
+ *
  * @brief Unit tests for the read filestream function
  *
  * @test \b Test_Spiffs_FileStream_test_case_03     \n <b> 1) Positive cases: </b>
- *                                                  \n          Read an example string from a filestream opened in r 
+ *                                                  \n          Read an example string from a filestream opened in r
  *                                                              mode and verify that the read string matches the original one
  *                                                  \n <b> 2) Negative cases (check for proper error codes): </b>
  *                                                  \n          Try to pass NULL as a read buffer
  *                                                  \n          Try to pass 0 as the read length
- *                                                  \n          Try to pass NULL as a read buffer and 0 as the read length 
+ *                                                  \n          Try to pass NULL as a read buffer and 0 as the read length
  * @}
  */
 seos_err_t test_readFileStream(FileStreamFactory* fsFactory);
