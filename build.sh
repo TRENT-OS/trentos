d403237412327e313a28a3047ba3b3280486fe0a
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.com>
2020-06-16 13:16:18 +0200

    [NEW] Add ProxyNvm component
    Fixes:
    SEOS-1092

--- build.sh
+++ build.sh
@@ -42,6 +42,7 @@ WELL_KNOWN_PROJECTS=(
     test_config_server_fs_backend,src/tests/test_config_server_fs_backend
     test_keystore,src/tests/test_keystore
     test_network_api,src/tests/test_network_api
+    test_storage_interface,src/tests/test_storage_interface
     test_tls_api,src/tests/test_tls_api
     test_tlsserver,src/tests/test_tlsserver
 
