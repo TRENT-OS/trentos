8366377bf6407f3ce7034f63da00463d657e1739
Axel Heider <axel.heider@hensoldt.net>
2022-11-17 12:45:40 +0100

    test seL4_DebugNameThreadFmt()

--- src/native/hello_world/main.c
+++ src/native/hello_world/main.c
@@ -7,12 +7,21 @@
 //------------------------------------------------------------------------------
 
 #include <stdio.h>
-
+#include <sel4/sel4.h>
 
 //------------------------------------------------------------------------------
 int main(void)
 {
-    printf("Hello, world!\n");
+    seL4_DebugNameThreadFmt(seL4_CapInitThreadTCB, "%s-%d", "root", seL4_CapInitThreadTCB);
+    seL4_DebugDumpScheduler();
+
+    // ToDo: fix test runner to flush logs to console when a line matched or
+    //       the whole test has passed. Otherwise we may not see the output
+    //       as the log monitor thread gets killed before it can print things.
+    //       test has passed. Quick-hack is no print the magic line that makes
+    //       the test pass.
+    //
+    // printf("Hello, world!\n");
 
     return 0;
 }
