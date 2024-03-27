f3ab619325f5b90fa403c85704beacaf9841528a
Felix Schladt <felix.schladt@hensoldt.net>
2024-03-27 12:29:22 +0100

    [DEV] Update Copyright
    Change files to GPLv2 or Commercial licensing

--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@
 #
 # Build script
 #
-# Copyright (C) 2019-2022, HENSOLDT Cyber GmbH
+# Copyright (C) 2019-2024, HENSOLDT Cyber GmbH
+# 
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# For commercial licensing, contact: info.cyber@hensoldt.net
 #
 #-------------------------------------------------------------------------------
 
--- src/native/hello_world/main.c
+++ src/native/hello_world/main.c
@@ -2,7 +2,11 @@
 //
 // seL4 native hello world application
 //
-// Copyright (C) 2019, HENSOLDT Cyber GmbH
+// Copyright (C) 2019-2024, HENSOLDT Cyber GmbH
+// 
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+// For commercial licensing, contact: info.cyber@hensoldt.net
 //
 //------------------------------------------------------------------------------
 
